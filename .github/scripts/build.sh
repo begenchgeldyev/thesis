b3aa3e67b64bf2276ca486a6a7e93082fd942318
Michael Pozhidaev <msp@luwrain.org>
2026-04-08 01:45:30 +0700

    UML support

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,5 +1,21 @@
 #!/bin/bash -e
 
+export LANG=ru_RU.UTF-8
+
+pushd /etc/ImageMagick-* > /dev/null
+sed -i -e s:'</policymap>':"  <policy domain="\""coder"\"" rights="\""read | write"\"" pattern="\""EPS"\"" />\n  <policy domain="\""coder"\"" rights="\""read | write"\"" pattern="\""PDF"\"" />\n</policymap>": file
+popd > /dev/null
+
+find -iname '*.uml' -print0 | xargs --null -n 1 java -jar plantuml.jar
+find -iname '*.png' | while read i; do
+    PDF="${i/.png/.pdf}"
+    echo "Converting $i to $PDF"    
+    convert "$i" "$PDF"
+    pdfcrop "$PDF"
+    rm -f "$PDF"
+    mv "${i/.png/-crop.pdf}" "$PDF"
+done
+
 for i in 1 2 3; do
     pdflatex -interaction=batchmode  main.tex
 done
